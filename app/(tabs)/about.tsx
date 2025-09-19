83d290b3107bf9efeff527799f5d2f832b024923
Joshua Cotton <jicotton123@outlook.com>
2025-09-19 02:14:22 -0500

    Minor Change
    - Update to About page.

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -9,6 +9,7 @@ import Animated, {
 
 import Father from "@/components/my-components/About-Img";
 import Backdrop from "@/components/my-components/Backdrop";
+import BasicForm from "@/components/my-components/Basic-Form";
 import Header from "@/components/my-components/Header";
 import SplitText from "@/components/SplitText/SplitText";
 
@@ -82,6 +83,66 @@ Community: Fostering a sense of belonging and connection for all residents`
         <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
           <Text style={[styles.subtitle, {fontSize: (isTablet ? 35 : 20), width: (isTablet ? '50%' : '90%')}]}>{values}</Text>
         </View>
+
+        <View>
+          <Text style={{ color: "#FFF7DE", alignSelf: 'center', fontSize: 56, fontWeight: 'bold', }}>Shared Housing</Text>
+        </View>
+        
+        <View>
+          <Text style={{ color: "#FFF7DE", alignSelf: 'center', fontSize: 20, padding: 45}}>Hank's House is dedicated to providing an all-inclusive affordable shared living home to all those who are needing a new start. We offer a safe and clean living spaces. Let us help you find your place to call...Home.</Text>
+        </View>
+
+        <View>
+          <Text style={{ color: "#FFF7DE", textAlign: 'center', fontSize: 28, fontWeight: 'bold', width: '100%', padding: 20, backgroundColor: 'rgba(255, 255, 255, 0.0)', borderTopLeftRadius: 100, borderTopRightRadius: 100 }}>Starting at $975 per month!!!</Text>
+        </View>
+        
+        <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', backgroundColor: 'rgba(255, 255, 255, 0.0)', borderBottomLeftRadius: 100, borderBottomRightRadius: 100}}>
+          <View>
+            <View style={{ padding: 10 }}>
+              <Text style={[styles.columns, { fontWeight: 'bold' }]}>We Offer:</Text>
+              <Text style={styles.columns}>• Fully Furnished Rooms w/ TVs</Text>
+              <Text style={styles.columns}>• Wi-Fi and utilities included.</Text>
+              <Text style={styles.columns}>• No Credit Check</Text>
+              <Text style={styles.columns}>• Do Not Need 3x income</Text>
+              <Text style={styles.columns}>• No admin fees</Text>
+              <Text style={styles.columns}>• Walking distance to bus stops</Text>
+            </View>
+
+            <View style={{ padding: 10 }}>
+              <Text style={[styles.columns, { fontWeight: 'bold' }]}>Payments Accepted:</Text>
+              <Text style={[styles.columns, { fontWeight: 'bold' }]}>• SSI/SSDI</Text>
+              <Text style={[styles.columns, { fontWeight: 'bold' }]}>• VA Disability</Text>
+              <Text style={[styles.columns, { fontWeight: 'bold' }]}>• Housing Vouchers</Text>
+              <Text style={[styles.columns, { fontWeight: 'bold' }]}>• Government Assistance</Text>
+            </View>
+          </View>
+          
+          <View>
+            <View style={{ padding: 10 }}>
+              <Text style={[styles.columns, { fontWeight: 'bold' }]}>What to Expect:</Text>
+              <Text style={styles.columns}>• Affordability</Text>
+              <Text style={styles.columns}>• Safe Supportive Environment</Text>
+              <Text style={styles.columns}>• Washer/Dryer Available</Text>
+              <Text style={styles.columns}>• Linen/Towels Provided</Text>
+            </View>
+
+            <View style={{ padding: 10 }}>
+              <Text style={[styles.columns, { fontWeight: 'bold' }]}>Who We Service:</Text>
+              <Text style={styles.columns}>• Displaced Adults Seeking Stability</Text>
+              <Text style={styles.columns}>• Re-Entry</Text>
+              <Text style={styles.columns}>• Seniors</Text>
+              <Text style={styles.columns}>• Veterans</Text>
+              <Text style={styles.columns}>• Homeless</Text>
+              <Text style={styles.columns}>• Fixed Income</Text>
+              <Text style={styles.columns}>• Single Moms/Dads</Text>
+            </View>
+          </View>
+        </View>
+        
+        <View style={{ width: '50%', alignSelf: 'center' }}>
+          <Text style={styles.heading}>Contact Us Today</Text>
+          <BasicForm isTablet/>
+        </View>
       </ReanimatedScrollView>
     </View>
   );
@@ -109,6 +170,11 @@ const styles = StyleSheet.create({
     flexWrap: "wrap",
     color: "#FFF7DE",
   },
+  columns: {
+    color: "#FFF7DE",
+    fontSize: 20,
+  },
+  heading: { fontSize: 33, marginTop: 20, marginBottom: 20, alignSelf: 'center', fontFamily: 'Playfair', color: "#FFF7DE", },
 });
 
 export default About;
